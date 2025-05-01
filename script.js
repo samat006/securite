f8bd48f0df7699e3df02eb7e681c7f40855e833f
seck <main>
2025-05-01 22:08:45 +0200

    compteur node

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ document.getElementById("loginForm").addEventListener("submit", function (event)
     }
   });
   
+
+
+  const btn = document.getElementById("btnConnexion");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault(); // Empêche la soumission réelle
+
+  fetch("http://localhost:3000/clicks", {
+    method: "POST"
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log("Nombre total de clics :", data.clicks);
+      alert("il ya pas de cooperation cest just pour voir le nombre d'etudaint qui tombe sur le piege. nombre d'etudiant : " + data.clicks);
+    })
+    .catch(err => console.error("Erreur côté client :", err));
+});
